6fb96754a0d42dfb5806798f19eebf17a197720d
Boutrik <alexandreboutrik@protonmail.ch>
2024-06-06 23:24:49 -0300

    fix bugs, add test case and improve dir layout

---
+++ Stacks and Queues/QuEuE StAcKeRs/Ada/src/utility.ads
@@ -0,0 +1,16 @@
+with Data_Structure;                    use Data_Structure;
+
+package Utility is
+
+    function Get_Next_Word (Source: String; Start_Index: in out Integer)
+        return String;
+
+    procedure Chega (Wqueue: in out Two_Queue; Ticket: String; Name: String);
+
+    procedure Vende (Wqueue: in out Two_Queue; Window: in out Two_Stack;
+        NumStr: String; Sold: in out Integer);
+
+    procedure Carrega (Window: in out Two_Stack; End_Str: String;
+        Start_Str: String; NumStr: String);
+
+end Utility;
